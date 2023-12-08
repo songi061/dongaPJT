79e8249d769792722a518fc257ffee3c1de1ff7c
songi <msongi061@gmail.com>
2023-12-08 16:37:44 +0900

    Web Frame

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -5,4 +5,11 @@ from mainApp import views
 urlpatterns = [
     #http://127.0.0.1:8000/
     path("", views.index, name = 'index'),
+    path("scalp/", views.scalp),
+    path("shampoo/", views.shampoo),
+    path("my/", views.my),
+    path("login/", views.login),
+    path("join/", views.join),
 ]
+
+
--- rootWEB/mainApp/views.py
+++ rootWEB/mainApp/views.py
@@ -8,3 +8,28 @@ from django.core.paginator import Paginator
 def index(request) :
     print('debug >>> mainApp /index')
     return render(request, 'mainpage/index.html')
+
+
+def scalp(request) :
+    print('debug >>> mainApp /scalp')
+    return render(request, 'mainpage/scalp.html')
+
+
+def shampoo(request) :
+    print('debug >>> mainApp /shampoo')
+    return render(request, 'mainpage/shampoo.html')
+
+
+def my(request) :
+    print('debug >>> mainApp /my')
+    return render(request, 'mainpage/my.html')
+
+
+def login(request) :
+    print('debug >>> mainApp /login')
+    return render(request, 'mainpage/login.html')
+
+
+def join(request) :
+    print('debug >>> mainApp /join')
+    return render(request, 'mainpage/join.html')
